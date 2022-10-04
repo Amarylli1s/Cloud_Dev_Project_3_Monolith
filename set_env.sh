3c9fdd5ff71af542bd483336806f6664e939ec91
DSdude <ayoubjbili@gmail.com>
2022-10-04 06:00:10 +0100

    added what's left

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=zzzzzz31
-export POSTGRES_HOST=database-1.cz0afmyamg0h.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.cqgcndtecfvw.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::papicha31
+export AWS_BUCKET=papicha31
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
